71b7581b5db5379938ba710d2c9c635d9ba99921
atomo55 <>
2019-11-19 16:20:10 +0900

    3.6.2まで

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-    protect_from_forgery with: :exception
-
-    def hello
-        render html: "hello,world!"
-    end
-=======
   protect_from_forgery with: :exception
 
   def hello
     render html: "hello,world!"
   end
->>>>>>> sample_app-static-pages
 end
